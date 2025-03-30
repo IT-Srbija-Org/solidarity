dd0d5d37f1c20b4070bac80bf2641771162f57e0
Predrag Leka <predrag@getsquire.com>
2025-03-30 21:23:07 +0200

    * Bank account number validation implemented

--- packages/Educator/src/Validator/Educator.php
+++ packages/Educator/src/Validator/Educator.php
@@ -42,6 +42,11 @@ class Educator implements ValidatorInterface
     {
         $valid = true;
 
+        if (!$this->validateAccountNumber($data['accountNumber'])) {
+            $this->messages['general'][] = 'Uneti broj žiro računa nije ispravan.';
+            $valid = false;
+        }
+
         if (!$this->csrf->validate($data)) {
             $this->messages['general'][] = 'Stranice je istekla, probajte ponovo.';
             $valid = false;
@@ -58,5 +63,36 @@ class Educator implements ValidatorInterface
     public function getMessages(): array
     {
         return $this->messages;
+    }
+
+    /**
+     * @param string $accountNumber
+     *
+     * @return bool
+     */
+    private function validateAccountNumber(string $accountNumber) : bool
+    {
+        $controlNumber = $this->mod97(substr($accountNumber, 0, -2));
+
+        return str_pad($controlNumber, 2, '0', STR_PAD_LEFT) === substr($accountNumber, -2);
+    }
+
+    /**
+     * @param string $accountNumber
+     * @param int    $base
+     *
+     * @return int
+     */
+    private function mod97(string $accountNumber, int $base = 100) : int
+    {
+        $controlNumber = 0;
+
+        for ($x = strlen($accountNumber)-1; !($x < 0); $x--) {
+            $num = (int)$accountNumber[$x];
+            $controlNumber = ($controlNumber + ($base * $num)) % 97;
+            $base = ($base * 10) % 97;
+        }
+
+        return 98 - $controlNumber;
     }
 }
