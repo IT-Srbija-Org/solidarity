998357fd02f4d9e623f25f25d42e4219f23b1054
Predrag Leka <predrag@getsquire.com>
2025-03-31 13:54:19 +0200

    * Cyrillic to latin transliterator implemented

--- packages/Delegate/src/Filter/Delegate.php
+++ packages/Delegate/src/Filter/Delegate.php
@@ -2,18 +2,18 @@
 
 namespace Solidarity\Delegate\Filter;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Laminas\Filter\ToInt;
 use Skeletor\Core\Filter\FilterInterface;
-use Skeletor\User\Service\Session;
 use Volnix\CSRF\CSRF;
-use Laminas\I18n\Filter\Alnum;
 use Skeletor\Core\Validator\ValidatorException;
 
 class Delegate implements FilterInterface
 {
 
-    public function __construct(private \Solidarity\Delegate\Validator\Delegate $validator)
+    public function __construct(
+        private \Solidarity\Delegate\Validator\Delegate $validator,
+        private \Solidarity\Transliterator\Service\Transliterator $transliter
+    )
     {
     }
 
@@ -24,22 +24,23 @@ class Delegate implements FilterInterface
 
     public function filter($postData): array
     {
-        $alnum = new Alnum(true);
         $int = new ToInt();
 
         $data = [
             'id' => (isset($postData['id'])) ? $int->filter($postData['id']) : null,
-            'name' => $postData['name'],
+            'name' => $this->transliter->transliterate($postData['name']),
             'email' => $postData['email'],
-            'city' => $postData['city'],
+            'city' => $this->transliter->transliterate($postData['city']),
             'phone' => $postData['phone'],
-            'verifiedBy' => (isset($postData['verifiedBy'])) ? $postData['verifiedBy'] : '',
+            'verifiedBy' => isset($postData['verifiedBy'])
+                ? $this->transliter->transliterate($postData['verifiedBy'])
+                : '',
             'count' => $postData['count'],
             'formLinkSent' => (isset($postData['formLinkSent'])) ? $postData['formLinkSent'] : 0,
             'countBlocking' => $postData['countBlocking'],
-            'schoolType' => $postData['schoolType'],
-            'schoolName' => $postData['schoolName'],
-            'comment' => $postData['comment'],
+            'schoolType' => $this->transliter->transliterate($postData['schoolType']),
+            'schoolName' => $this->transliter->transliterate($postData['schoolName']),
+            'comment' => $this->transliter->transliterate($postData['comment']),
             'status' => (isset($postData['status'])) ? $postData['status'] : 1,
 //            'createdAt' => $postData['createdAt'],
 //            'updatedAt' => $postData['updatedAt'],
